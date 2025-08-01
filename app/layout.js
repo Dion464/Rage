9a215f13906306979e188f4cc8bd8f0bc8ef6d5a
Dion Curri <curridion31@gmail.com>
2025-08-01 14:28:31 +0200

    Add Facebook Pixel tracking script to layout for enhanced analytics and user tracking capabilities.

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 import Navigation from './components/Navigation';
+import Script from 'next/script';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -13,6 +14,28 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3811009842530661');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=3811009842530661&ev=PageView&noscript=1"
+          />
+        </noscript>
         <Navigation />
         {children}
         <Analytics />
